54b5cc6b82d38219e4cda84fd52e9fca5f9624db
jealchavi <55636846+jealchavi@users.noreply.github.com>
2019-11-14 21:14:46 -0500

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -18,7 +18,65 @@ $(document).ready(function(){
         });
         
     });
- 
+
     
-    
+
+    //Creamos mediante el formulario un evento de tipo submit
+    /* $('#registro-frm').submit(function(e){
+
+        e.preventDefault();
+        //metodo post en jquery recibe 3 parametros: url, objeto, funcion anonima 
+        var usuario={
+            nombre: $("input[name='nombre']").val(),
+            web:    $("input[name='web']").val()
+        }
+
+        console.log(usuario);
+        
+        //Crearemos un usuario usando el metodo post
+        $.post($(this).attr('action'),usuario,function(response){
+            console.log(response);
+            
+        }).done(function(){
+            alert('usuario correcto');
+        });
+
+        return false;
+    }); */
+
+
+    //Hacer una peticion ajax usando el metodo ajax. es el mejor de todos
+    $('#registro-frm').submit(function(e){
+
+        e.preventDefault();
+
+        var usuario={
+            nombre: $("input[name='nombre']").val(),
+            web:    $("input[name='web']").val()
+        }
+
+
+        $.ajax({
+            //type indica el metodo
+            type:'post',
+            //url adonde enviaremos los datos
+            url: $(this).attr('action'),
+            //data hace alusion al objeto que se va a enviar
+            data: usuario,
+            //mediante beforeSend se le puede indicar lo que puede hacer mientras se envia
+            beforeSend: function(){
+                console.log('Enviando usuario');
+            },
+            //mediante success le podemos indicar lo que hara en caso de enviar los datos
+            success:function(){
+                console.log(usuario);
+            },
+            error:function(){
+                console.log('error al enviar datos');
+            }
+        });
+
+        return false;
+    });
+  
 });
